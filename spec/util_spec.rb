247c21ef1289e0be13a3d973ce1054d689058d1d
Wincent Colaiuta <win@wincent.com>
2007-12-24 14:57:44 +0100

    Add specs for missing command line switches
    Test case where --strings is supplied without --info and vice versa.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -19,8 +19,46 @@ describe 'no command line switches' do
   end
 end
 
+describe 'missing command line switches' do
+  describe '--info without --strings' do
+    before(:all) do
+      @result = Util.run('--info', 'foo')
+    end
+
+    it 'should exit with a non-zero status' do
+      @result.exitstatus.should_not == 0
+    end
+
+    it 'should show usage information' do
+      @result.stderr.should =~ /Usage/
+    end
+
+    it 'should complain about missing --strings switch' do
+      @result.stderr.should =~ /--strings option is required/
+    end
+  end
+
+  describe '--strings without --info' do
+    before(:all) do
+      @result = Util.run('--strings', 'foo')
+    end
+
+    it 'should exit with a non-zero status' do
+      @result.exitstatus.should_not == 0
+    end
+
+    it 'should show usage information' do
+      @result.stderr.should =~ /Usage/
+    end
+
+    it 'should complain about missing --strings switch' do
+      @result.stderr.should =~ /--info option is required/
+    end
+  end
+end
+
 describe 'conflicting command line switches' do
-  describe '--merge and -info' do
+  describe '--merge and --info' do
     before(:all) do
       @result = Util.run('--merge', 'foo', '--info', 'bar')
     end

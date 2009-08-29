7ffabdc72e0d9f770b3ac31393bd63069ff68c5e
Wincent Colaiuta <win@wincent.com>
2009-08-29 12:16:39 +0200

    distro.sh: implement tag checking
    This is the first stage of the distro script. Issue a warning if current
    HEAD is not a signed/annotated commit.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- distro.sh
+++ distro.sh
@@ -1,7 +1,23 @@
 #!/bin/sh -e
 # Copyright 2009 Wincent Colaiuta.
 
-# print warning if current HEAD is not a signed tag
+. "buildtools/Common.sh"
+
+# print warning if current HEAD is not a signed/annotated tag
+TAGGED=""
+
+# for signed tags this will be "2.0" or similar
+# for unsigned tags or untagged commits will be "2.0-5-g08867ea" or similar
+HEAD=$(git describe)
+for TAG in $(git tag)
+do
+  if [ $TAG = $HEAD ]; then
+    TAGGED=$TAG
+  fi
+done
+if [ -z "$TAGGED" ]; then
+  warn "current HEAD is not tagged/annotated, but it should be for official releases"
+fi
 
 # prep binary -> zip installer pkg
 

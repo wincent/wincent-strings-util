dd770463c51d0fdc73715d101e98aef9a317c24f
Wincent Colaiuta <win@wincent.com>
2009-08-29 12:00:41 +0200

    Add schema for "distro.sh" script
    At this stage just a plan of what we want the script to do eventually.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ distro.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+# Copyright 2009 Wincent Colaiuta.
+
+# print warning if current HEAD is not a signed tag
+
+# prep binary -> zip installer pkg
+
+# prep source archive
+# including submodules
+
+# prep release notes
+
+# prep plaintext version of manpage
